78e2caf7076f7e84e13fce5c6009e0cbbf2efacb
Dennis Schill <73108838+Soldi00@users.noreply.github.com>
2022-05-17 21:02:20 +0200

    Uppdaterad

--- java/src/model/IMemberService.java
+++ java/src/model/IMemberService.java
@@ -6,24 +6,24 @@ public interface IMemberService {
 
 
     /**
-     * Get all members.
-     * @return Member[].
+     * Get all members
+     * @return Member[]
      */
 
     Member[] getAllMembers();
 
     /**
-     * Get specific member by ID.
+     * Get specific member by ID
      * @param memberID
-     * @return Member.
+     * @return Member
      */
 
     Member getTheMember(int memberID);
 
     /**
-     * Update member by ID.
+     * Update member by ID
      * @param memberID
-     * @return boolean.
+     * @return boolean
      */
 
     boolean updateMember(int memberID);

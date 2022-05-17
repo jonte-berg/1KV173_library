f2b40e16749179d80916a523404ecae088761b07
Dennis Schill <73108838+Soldi00@users.noreply.github.com>
2022-05-17 21:02:20 +0200

    Uppdaterad

--- java/src/model/IMemberManager.java
+++ java/src/model/IMemberManager.java
@@ -1,4 +1,40 @@
 package model;
 
+import java.time.LocalDate;
+
 public interface IMemberManager {
+
+    /**
+     * Add member
+     * @param newMember
+     * @return boolean
+     */
+
+    boolean addMember(Member newMember);
+
+    /**
+     * Delete member
+     * @param memberID
+     * @return boolean
+     */
+
+    boolean deleteMember(int memberID);
+
+    /**
+     * Suspend member
+     * @param start
+     * @param end
+     * @return boolean
+     */
+
+    boolean suspendMember(LocalDate start, LocalDate end);
+
+
+    /**
+     * Search member
+     * @param memberID
+     * @return boolean
+     */
+    boolean searchForMember(int memberID);
+
 }

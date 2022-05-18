8b5f4e3deadc06410f8533000798cf93ed8ea070
Jonathan Berg (jb223pt) <jb223pt@student.lnu.se>
2022-05-18 09:11:29 +0200

    Merge remote-tracking branch 'origin/main'

--- java/src/model/Book.java
+++ java/src/model/Book.java
@@ -1,4 +1,60 @@
 package model;
 
 public class Book {
+
+    private int isbn;
+    private String title;
+    private String genre;
+    private int quantity;
+    private boolean isAvailable;
+
+    public Book(){}
+
+    public Book(int isbn, String title, String genre, int quantity, boolean isAvailable){
+        this.isbn=isbn;
+        this.title=title;
+        this.genre=genre;
+        this.quantity=quantity;
+        this.isAvailable=isAvailable;
+    }
+
+    public int getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(int isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public boolean isAvailable() {
+        return isAvailable;
+    }
+
+    public void setAvailable(boolean available) {
+        isAvailable = available;
+    }
 }

14221ff6c6eafe6477703146cc4c519e204a9ae5
Robin <robin.sjoberg.g@gmail.com>
2022-05-18 14:09:10 +0200

    LoanManagerTest och lite uppdateringar

---
+++ java/src/model/LoanManagerTest.java
@@ -0,0 +1,30 @@
+package model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LoanManagerTest {
+
+    @org.junit.jupiter.api.Test
+    void searchForBookISBN() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void searchForBookTitle() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void addLoan() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void deleteLoan() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void loanItems() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void issueFine() {
+    }
+}

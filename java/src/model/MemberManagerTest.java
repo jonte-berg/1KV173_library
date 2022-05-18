350419440ea123634b105b628260fcbf2adfbc00
Dennis Schill <73108838+Soldi00@users.noreply.github.com>
2022-05-18 10:25:04 +0200

    Uppdaterad

--- java/src/model/MemberManagerTest.java
+++ java/src/model/MemberManagerTest.java
@@ -4,14 +4,24 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertNull;
+
 class MemberManagerTest {
+
+   /* MemberService memberService = mock(MemberService.class); */
+    MemberManager mg = new MemberManager();
 
     @BeforeEach
     void setUp() {
+        System.out.println("Setting it up! ....");
+        /* mg = new MemberManager(memberService); */
     }
 
     @AfterEach
     void tearDown() {
+        System.out.println("Running: tearDown! ....");
+        mg = null;
+        assertNull(mg);
     }
 
     @Test

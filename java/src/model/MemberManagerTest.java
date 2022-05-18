b97569723cfd4fac25b359be1af22df172b2f6f7
Dennis Schill <73108838+Soldi00@users.noreply.github.com>
2022-05-18 08:10:18 +0200

    Uppdaterad

---
+++ java/src/model/MemberManagerTest.java
@@ -0,0 +1,34 @@
+package model;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class MemberManagerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void addMember() {
+    }
+
+    @Test
+    void deleteMember() {
+    }
+
+    @Test
+    void suspendMember() {
+    }
+
+    @Test
+    void searchForMember() {
+    }
+
+
+}

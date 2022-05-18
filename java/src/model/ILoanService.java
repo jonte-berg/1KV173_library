93a02ee89f2e61201a8834f4867a6402701d11dc
Robin <robin.sjoberg.g@gmail.com>
2022-05-18 10:18:35 +0200

    LoanManager och LoanService class

--- java/src/model/ILoanService.java
+++ java/src/model/ILoanService.java
@@ -1,4 +1,58 @@
 package model;
 
+import java.time.LocalDate;
+import java.util.List;
+
 public interface ILoanService {
+
+
+
+    /*
+    Get loans from database that is in the given startDate and endDate.
+    @para startDate
+    @para endDate
+    @return Loan[]
+     */
+
+    Loan [] getAllLoans(LocalDate startDate, LocalDate endDate);
+
+
+
+    /*
+    Get all loan that belongs to a specifik member.
+    @para memberID
+    @return Loan[]
+     */
+
+    Loan [] getLoanByMember(int membersID);
+
+
+
+    /*
+    Get all books that is in the database.
+    @return Book[]
+     */
+
+    Book [] getAllBooks();
+
+
+
+    /*
+    get a specifik book by its ISBN number.
+    @para isbnNr
+    @return Book
+     */
+
+    Book getBookById(int isbnNr);
+
+
+
+    /*
+    get a specifik book by its Title.
+    @para title
+    @return Book
+     */
+
+    Book getBookByTitle(String title);
+
 }

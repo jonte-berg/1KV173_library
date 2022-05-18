c642a436fe5208d3cc397330a6476e61fb78bcfc
Dennis Schill <73108838+Soldi00@users.noreply.github.com>
2022-05-18 08:10:18 +0200

    Uppdaterad

--- java/src/model/MemberManager.java
+++ java/src/model/MemberManager.java
@@ -3,7 +3,6 @@ package model;
 import java.time.LocalDate;
 
 public class MemberManager implements IMemberManager {
-
     @Override
     public boolean addMember(Member newMember) {
         return false;

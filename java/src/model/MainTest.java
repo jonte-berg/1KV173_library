8794350a4ae79b93f1ad06ef21cb632f6112c06e
Jonathan Berg (jb223pt) <jb223pt@student.lnu.se>
2022-05-19 11:05:30 +0200

    Merge remote-tracking branch 'origin/main'

--- java/src/model/MainTest.java
+++ java/src/model/MainTest.java
@@ -51,6 +51,11 @@ public class MainTest {
         }
 
 
+        MemberService a = new MemberService();
+       ArrayList<Member> b= a.getAllMembers();
+
+        System.out.println(b.get(0).getlName());
+
 
 
         /*--------------------------------------------DB connection--------------------------------------------------------*/

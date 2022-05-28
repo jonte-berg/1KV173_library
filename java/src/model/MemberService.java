d0a30891ae6fb977332a940e7dd7900559a62b69
Dennis Schill <73108838+Soldi00@users.noreply.github.com>
2022-05-28 11:48:16 +0200

    Uppdaterat MemberService

--- java/src/model/MemberService.java
+++ java/src/model/MemberService.java
@@ -120,38 +120,6 @@ public class MemberService implements IMemberService {
         return false;
     }
 
-
-    public boolean updateMember(Member member){
-
-        loadDrivers();
-
-        try (Connection conn = DriverManager.getConnection(
-                "jdbc:mysql://library-1ik173.mysql.database.azure.com:3306/library1ik173?useSSL=true",
-                "gruppD",
-                "Q1w2e3r4t5")) {
-          //update member SET total_Warnings=1 where memberID=1001	1 row(s) affected
-            //Rows matched: 1  Changed: 1  Warnings: 0	0.016 sec
-            PreparedStatement ps = conn.prepareStatement(
-                    "UPDATE Member SET suspended = ?, total_Warnings = ? WHERE memberID = ?");
-
-            ps.setInt(1,member.isSuspended());
-            ps.setInt(2,member.getWarnings());
-            ps.setInt(3,member.getId());
-            ps.execute();
-
-
-            System.out.println("Member with ID: "+member.getId()+ " has been updated!");
-            return true;
-
-        } catch (SQLException ex) {
-
-            System.out.println("Something went wrong...");
-        }
-
-
-        return false;
-    }
-
     public static void loadDrivers() {
         try {                                                                           //Läser in drivrutinerna (behövs egentligen inte då det sker automatiskt, men kan vara bra att få ett tecken på att de är laddade)
             Class.forName("com.mysql.cj.jdbc.Driver");
